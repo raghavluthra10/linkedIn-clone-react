8afc8046280bd90bb8d6fe732fe4ca536cd56334
raghav786 <58934695+raghav786@users.noreply.github.com>
2021-05-20 18:31:46 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import Login from './components/Login';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+ 
 
 function App() {
   return (
     <div className="app">
-      <h1> Let's start with the LinkedIn clone. </h1>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Login  />
+          </Route>
+          <Route exact path='/home'>
+            <Header  />
+            <Home  />
+          </Route>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
 
 export default App;
+
+
+// 1:47:00
